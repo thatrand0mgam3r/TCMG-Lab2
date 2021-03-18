05127851569b4a8ca753e2a498b9ca15118c1c6d
thatrand0mgam3r <77948235+thatrand0mgam3r@users.noreply.github.com>
2021-03-18 11:40:39 -0500

    Update main.py
    Added all files into a main set up, this way all files run on app.route.
    
    Hopefully this works.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, Response
+import hashlib
 
 app = Flask(__name__)
 
@@ -31,9 +32,57 @@ app = Flask(__name__)
         return jsonify()
 
 
-@app.route('/is-prime/<int:n>')
-def prime_check(n):
+@app.route('/is-prime/<int:num>')
+    def is_prime(num):
+        if isinstance(num, int):
+    # show the post with the given id, the id is an integer
+        if num > 1:
+      # Iterate from 2 to n / 2
+            for i in range(2, int(num/2)+1):
+  
+          # If num is divisible by any number between
+          # 2 and n / 2, it is not prime
+                if (num % i) == 0:
+                    return False
+                    break
+            else:
+                return True
+            else:
+                return False
+        else:
+            return "Input is not an integer. You input a " + str(type(num))
+@app.errorhandler(404)
+def not_found(error):
+    return "Input is not an integer"    
     return
+
+# from flask import Flask
+# app = Flask(__name__)
+@app.route('/md5/<string:hash>')
+def MD5(hash):
+    user_input = input("Enter your sting here:")
+    hash_object = hashlib.md5(user_input.encode())
+    md5_hash = hash_object.hexdigest()
+    print("Your string was: ", user_input)
+    print("The MD5 hash equivalent of your sting is: ", md5_hash)
+
+    
+@app.route('/factorial/<int>')    
+def factorial(num):
+    if num == 1:
+            return num
+    else:
+        return num *factorial(num - 1)
+num = int(input("Input: "))
+if num < 0:
+    print("Factorial cannot be found for negative integer")
+elif num == 0:
+    print("Output: 1")
+else:
+    print("Output:",factorial(num))
+    
+    
+    
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000)
