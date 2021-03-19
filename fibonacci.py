83be128580fa31ee02c8ab04d58e632282ebf2aa
thatrand0mgam3r <77948235+thatrand0mgam3r@users.noreply.github.com>
2021-03-19 10:46:25 -0500

    Update fibonacci.py
    added comments to better understand the flow of work.

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,17 @@
+# from flask import Flask
+# app = Flask(__name__)
 
+# @app.route('/fibonacci/<int:n>')
 fib = [0,1]
 result = 0
-n =  int(input("What number would you like to go up to?  " ))
-while result < n:
+
+while result < n: 
+# will continue looping until the process reaches the same number as we input/ stop just before it reaches it
     result = fib[-1] + fib[-2]
+    #adds up the 2 previous spots together
     if (result < n):
-        fib.append(result)
+        fib.append(result)# will overwrite the original fib so it mathces the new data set
 if n > 0:
-    print(fib)
+    print(fib) 
 elif n <= 0:
     print("ERROR")
