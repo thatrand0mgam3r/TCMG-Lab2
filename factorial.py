9d56a9b439396cb483962ad4a47be2de7bb6c1ba
nhattn38 <63120388+nhattn38@users.noreply.github.com>
2021-04-14 20:38:21 -0500

    Corrected factorial.py

--- factorial.py
+++ factorial.py
@@ -1,13 +1,17 @@
 #from flask import Flask
 #app = FLask (__name__)
-@app.route("factorial/<integer>" 
-def factorial(num):
-    factorial=1
-    if num < 0:
-        return jsonify(input=num, output="Factorial cannot be found for negative number")
-    elif num == 0:
-       return jsonify(input=num, output=1)
+@app.route("factorial/<int:n>") 
+
+def factorial(n):
+    fact = 1
+    if n == 1:
+        print("input:", n, "output:", 1)
+    elif n == 0:
+        print("input:", n, "output:", 1)
+    elif n < 0:
+        print("Factorial cannot be found for negative integer")
     else:
-        for i in range(1, num+1):
-           factorial = factorial*i
-       return jsonify(input=num, output=factorial) 
+        for i in range(1, n + 1):
+            fact = fact * i
+        print("input:", n, "output:", fact)
+        
