b25d26fab6cbfae69fed91129037189a68031698
nhattn38 <63120388+nhattn38@users.noreply.github.com>
2021-04-14 20:55:48 -0500

    update factorial.py
    Had to take away strings for test to work

--- factorial.py
+++ factorial.py
@@ -5,13 +5,13 @@
 def factorial(n):
     fact = 1
     if n == 1:
-        print("input:", n, "output:", 1)
+        return 1
     elif n == 0:
-        print("input:", n, "output:", 1)
+        return 1
     elif n < 0:
         print("Factorial cannot be found for negative integer")
     else:
         for i in range(1, n + 1):
             fact = fact * i
-        print("input:", n, "output:", fact)
-        
+        return fact
+
